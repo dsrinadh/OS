daae3e09f33419a6b69fa7bec4867f757c78938d
dsrinadh <dharmapurisrinadhgoud@gmail.com>
2026-06-08 19:36:08 +0530

    Create About.js

---
+++ NodeJs/Week10/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a student management system built using React.</p>
+    </div>
+  );
+}
+
+export default About;
